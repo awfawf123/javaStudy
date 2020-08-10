f57a98c3c30735aa04aef527364dc7bad6cc5f6d
ojh <awfawf123@naver.com>
2020-08-10 11:09:02 +0900

    first commit

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello, welcome to the java world!");
+		System.out.println("hi hi hi");
+		int myAge = 25;
+		System.out.println("My Age is " + myAge);
+	}
+}
